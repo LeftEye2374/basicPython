a722736c8220767a4fe7707ff16e25e61dce34ed
LeftEye2374 <shinnn2374@mail.ru>
2026-07-22 14:21:05 +0300

    fix index mismatch on task removal, handle invalid menu option

--- todo/main.py
+++ todo/main.py
@@ -9,27 +9,28 @@ def start():
     print('1. Add a task')
     print('2. Remove a task')
     print('3. Show all tasks')
-    option = int(input())
 
-
-
-    if(option == 1):
-        name = str(input('What name of the task:'))
-        description = str(input('What description of the task:'))
-        deadLine = str(input('What deadline of the task:'))
-        task = Task(name, description, deadLine)
-        taskList.append(task)
-    elif(option == 2):
-        index = int(input('What index of the task:'))
-        taskList.pop(index)
-    elif(option == 3):
-        index = 1
-        for task in taskList:
-            print(f'Task number {index}')
-            task.getName()
-            task.getDescription()
-            task.getDeadLine()
-            index += 1
+    try:
+        option = int(input())
+        if(option == 1):
+            name = str(input('What name of the task:'))
+            description = str(input('What description of the task:'))
+            deadLine = str(input('What deadline of the task:'))
+            task = Task(name, description, deadLine)
+            taskList.append(task)
+        elif(option == 2):
+            index = int(input('What index of the task:'))
+            taskList.pop(index)
+        elif(option == 3):
+            index = 0
+            for task in taskList:
+                print(f'Task number {index}')
+                task.getName()
+                task.getDescription()
+                task.getDeadLine()
+                index += 1
+    except (IndexError, ValueError):
+        print('Invalid input')
 
 while(True):
     start()
