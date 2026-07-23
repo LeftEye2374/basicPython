619c3559edebb6642a90f7cbe31b2550ae097187
LeftEye2374 <shinnn2374@mail.ru>
2026-07-23 17:36:45 +0300

    Objects and classes

--- OOP/classes/read_data.py
+++ OOP/classes/read_data.py
@@ -3,4 +3,24 @@ import json
 with open('data/egypt.json', 'r') as country_file:
     file_data = json.load(country_file)
 
-print(file_data['country'])
+
+class CountryData:
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file_data = self.read_data()
+        self.country = self.file_data['country']
+        self.max_temp = self.file_data['max_temp']
+        self.min_temp = self.file_data['min_temp']
+
+
+    def read_data(self):
+        with open(self.file_path, 'r') as country_file:
+            return json.load(country_file)
+
+
+egypt = CountryData('data/egypt.json')
+print(egypt.file_data)
+print(egypt.country)
+print(egypt.max_temp)
+print(egypt.min_temp)
