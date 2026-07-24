501e345ca7d27acb6d65234201a93a7e433bde14
LeftEye2374 <shinnn2374@mail.ru>
2026-07-24 18:24:32 +0300

    create Iterator class

--- IteratorsAndGenerators/Generator.py
+++ IteratorsAndGenerators/Generator.py
@@ -6,4 +6,28 @@ def gen(n):
     for i in range(n):
         yield i ** 2
 
-print(sum(gen(n)))
+
+# Созданиек класса итератора
+class Iter:
+    def __init__(self, n):
+        self.n = n
+        self.current = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current += 1
+        if self.current >= self.n:
+            raise StopIteration()
+        return self.current
+
+
+iterator = Iter(5)
+
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
+print(iterator.__next__())
