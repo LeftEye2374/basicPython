004c26419edd9d4f75b6434a7e7ca82545113ad5
LeftEye2374 <shinnn2374@mail.ru>
2026-07-24 14:56:52 +0300

    play with iteration

---
+++ IteratorsAndGenerators/main.py
@@ -0,0 +1,11 @@
+s = 'Stroka'
+s_itr = iter(s)
+print(s_itr)
+
+print(s_itr.__next__())
+print(s_itr.__next__())
+print(s_itr.__next__())
+print(s_itr.__next__())
+print(s_itr.__next__())
+print(s_itr.__next__())
+print(s_itr.__next__())
