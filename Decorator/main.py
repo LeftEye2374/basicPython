8961bca7317638e81e946250ececd668ed166139
LeftEye2374 <shinnn2374@mail.ru>
2026-07-27 11:05:37 +0300

    timer deco

--- Decorator/main.py
+++ Decorator/main.py
@@ -1,4 +1,5 @@
 from typing import Callable
+import time
 
 # my_func = deco(my_function()) # Пример применения декоратора без @
 
@@ -12,7 +13,17 @@ def empty_deco(func : Callable):
         return res
     return wrapper
 
-@empty_deco
+def timer_deco(func : Callable):
+    def wrapper():
+        start = time.time()
+        res = func()
+        time.sleep(3) # Просто чтобы добавить времени
+        end = time.time()
+        print(f"Исполнение заняло - {end-start}")
+        return res
+    return wrapper
+
+@timer_deco
 def my_function():
     return 124
 
