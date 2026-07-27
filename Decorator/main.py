b02562bb450a53d3674d542a823dcd15ff59a5fd
LeftEye2374 <shinnn2374@mail.ru>
2026-07-27 11:02:32 +0300

    Empty deco

--- Decorator/main.py
+++ Decorator/main.py
@@ -1,7 +1,19 @@
-def deco():
-    ...
+from typing import Callable
 
-@deco
+# my_func = deco(my_function()) # Пример применения декоратора без @
+
+# Декоратор - это паттерн программирования который позволяет
+# добавлять новый функционал фукции, не видоизменяя эту функцию
+
+
+def empty_deco(func : Callable):
+    def wrapper():
+        res = func()
+        return res
+    return wrapper
+
+@empty_deco
 def my_function():
     return 124
 
+print(my_function())
