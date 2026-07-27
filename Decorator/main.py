905ec9dd114bec8d7c8158c6fd0659e1d5464b1a
LeftEye2374 <shinnn2374@mail.ru>
2026-07-27 11:39:51 +0300

    async deco

--- Decorator/main.py
+++ Decorator/main.py
@@ -1,5 +1,8 @@
 from typing import Callable
 import time
+from functools import wraps
+from typing import Coroutine
+import asyncio
 
 # my_func = deco(my_function()) # Пример применения декоратора без @
 
@@ -32,9 +35,35 @@ def parametrized_timer_deco(func : Callable):
         return res
     return wrapper
 
-@parametrized_timer_deco
+def limit_calls_deco(limit : int):
+    def wrapper(func : Callable):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            nonlocal limit # Используется в основном в декоратарах
+            if limit == 0:
+                print("Нельзя вызвать функцию")
+            res = func(*args, **kwargs)
+            limit -= 1
+            return res
+        return inner
+    return wrapper
+
+def async_function_deco(coroutine : Coroutine):
+    async def wrapper(*args, **kwargs):
+        res = await coroutine(*args, **kwargs)
+        return res
+    return wrapper
+
+
+@async_function_deco
+async def my_async_function():
+    await asyncio.sleep(0.5)
+    return 1
+
+@limit_calls_deco(2)
 def my_function(sleep_second : int):
+    """Очень важный докстринг"""
     time.sleep(sleep_second)
     return 124
 
-print(my_function(2))
+print(asyncio.run(my_async_function()))
